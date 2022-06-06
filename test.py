bede7dedcdd047b57c260b056e73ece579d472d7
Mark Damien Chinnasamy <markdamienchinnasamy@gmail.com>
2022-06-06 23:08:01 +0200

    chessboard map

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 """Test board format DB for chess Pieces"""
-player_1_NP = ["R1","Kn1","B1","Q","K","B2","Kn2","R2"]
-player_1_P  = ["P1","P2","P3","P4","P5","P6","P7","P8"]
+alphaClassList = ['A','B','C','D','E','F','G','H']
+class ChessTile:
+    def __init__(self, row:int, col:int):
+        self.name = alphaClassList[row] + ',' + str(col)
+        self.row = row
+        self.col = col
+        self.occ = False
 
-player_2_NP = ["R1","Kn1","B1","Q","K","B2","Kn2","R2"]
-player_2_P  = ["P1","P2","P3","P4","P5","P6","P7","P8"]
+def namingFunc():
+    player_1_NP = ["R1","Kn1","B1","Q","K","B2","Kn2","R2"]
+    player_1_P  = ["P1","P2","P3","P4","P5","P6","P7","P8"]
+
+    player_2_NP = ["R1","Kn1","B1","Q","K","B2","Kn2","R2"]
+    player_2_P  = ["P1","P2","P3","P4","P5","P6","P7","P8"]
+
+def boardMapCreate():
+    grid = [[0 for _ in range(8)] for _ in range(8)]
+    for i in range(8):
+        for j in range(8):
+            grid[i][j] = ChessTile(i,j)
+    return grid
+
+board = boardMapCreate()
+
